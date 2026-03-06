415692baf8839d7486c901abf92c1879338dd581
Rolando Fragela Herva <chokisoft@gmail.com>
2026-03-05 19:40:00 -0500

    Cambio en environment 05-03-2026

--- GoldBusiness.Client/src/environments/environment.development.ts
+++ GoldBusiness.Client/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7253/api',
-  //apiUrl: 'https://goldbusinesswebapi-dev.azurewebsites.net/api',
+  //apiUrl: 'https://localhost:7253/api',
+  apiUrl: 'https://goldbusinesswebapi-dev.azurewebsites.net/api',
   apiVersion: 'v2'
 };
